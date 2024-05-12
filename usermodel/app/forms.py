88365447fc894a54ccd331ce5c24a0e90132091d
AbhiRam1010 <141568371+AbhiRam1010@users.noreply.github.com>
2024-05-12 12:45:37 +0530

    Add files via upload

---
+++ usermodel/app/forms.py
@@ -0,0 +1,11 @@
+from django import forms 
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields=['first_name','last_name','email','username','password']
+        help_texts={'username':'unique userID'}
+        widgets={'password':forms.PasswordInput}
+        
+        
